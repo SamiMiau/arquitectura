a811d169162b6a09afe34251a6c7ce1aca634e70
unknown <natalia.sanchez@sansano.usm.cl>
2023-10-04 19:17:12 -0300

    rabbit and events

--- app/events.py
+++ app/events.py
@@ -11,18 +11,18 @@ class Emit:
 
     def connect(self):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='demo_04_message_broker')
+            pika.ConnectionParameters(host='inventory_message_broker')
         )
 
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange='teams',
+        self.channel.exchange_declare(exchange='inventory',
                                       exchange_type='topic')
 
     def publish(self, id, action, payload):
-        routing_key = f"team.{action}.{id}"
+        routing_key = f"inventory.{action}.{id}"
         message = json.dumps(payload)
 
-        self.channel.basic_publish(exchange='teams',
+        self.channel.basic_publish(exchange='inventory',
                                    routing_key=routing_key,
                                    body=message)
 
@@ -30,34 +30,34 @@ class Emit:
         self.connection.close()
 
 
-class Receive:
-    def __init__(self):
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='demo_04_message_broker')
-        )
+# class Receive:
+#     def __init__(self):
+#         self.connection = pika.BlockingConnection(
+#             pika.ConnectionParameters(host='demo_04_message_broker')
+#         )
 
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange='players',
-                                      exchange_type='topic')
+#         self.channel = self.connection.channel()
+#         self.channel.exchange_declare(exchange='players',
+#                                       exchange_type='topic')
 
-        self.channel.queue_declare('team_for_player_queue', exclusive=True)
-        self.channel.queue_bind(exchange='players',
-                                queue="team_for_player_queue",
-                                routing_key="player.delete.*")
+#         self.channel.queue_declare('team_for_player_queue', exclusive=True)
+#         self.channel.queue_bind(exchange='players',
+#                                 queue="team_for_player_queue",
+#                                 routing_key="player.delete.*")
 
-        self.channel.basic_consume(queue='team_for_player_queue',
-                                   on_message_callback=self.callback)
+#         self.channel.basic_consume(queue='team_for_player_queue',
+#                                    on_message_callback=self.callback)
 
-        self.channel.start_consuming()
+#         self.channel.start_consuming()
 
-    def callback(self, ch, method, properties, body):
-        body = json.loads(body)
-        logging.info(f"Good by {body['name']} 👋")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+#     def callback(self, ch, method, properties, body):
+#         body = json.loads(body)
+#         logging.info(f"Good by {body['name']} 👋")
+#         ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    def close(self):
-        self.connection.close()
+#     def close(self):
+#         self.connection.close()
 
 
-if __name__ == '__main__':
-    Receive()
+# if __name__ == '__main__':
+#     Receive()
--- app/main.py
+++ app/main.py
@@ -11,14 +11,14 @@ from fastapi import HTTPException
 from pydantic import BaseModel
 
 from .events import Emit
-from .events import Receive
+#from .events import Receive
 
 
 app = FastAPI()
 mongodb_client = MongoClient("inventory_mongodb", 27017)
 
-# emit_events = Emit()
-# threading.Thread(target=Receive).start()
+emit_events = Emit()
+#threading.Thread(target=Receive).start()
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
@@ -68,7 +68,7 @@ class User(BaseModel):
 @app.get("/")
 async def root():
     return {"Hello": "World"}
-#----------------------------------------------------------------------USER CRUD---------------------------------------------
+#----------------------------------------------------------------------USER---------------------------------------------
 @app.post("/newuser/{user_name}", response_model=User)
 def add_user(user_name: str) -> User:
     user = User(name=user_name, gold=1000)
@@ -83,20 +83,25 @@ def add_user(user_name: str) -> User:
     )
 
     #logging.info(f"✨ New team created: {new_team}")
-    #emit_events.send(inserted_id, "create", new_team.dict())
+    emit_events.send(inserted_id, "create", new_user.dict())
     return new_user
 
 @app.get("/getuser/{user_id}", response_model=User)
 def get_user(user_id: str)-> User:
     try:
         user_id = ObjectId(user_id)
+
         return User(
             **mongodb_client.inventory.users.find_one({"_id": user_id})
         )
     except (InvalidId, TypeError):
-        raise HTTPException(status_code=404, detail="Player not found")
+        raise HTTPException(status_code=404, detail="User not found")
 
-
+@app.get("/inventory", response_model=list[Item])
+def get_all_inventory(user_id: str)-> list[Item]:
+    user = get_user(user_id)
+    items = user.items
+    return items
 #--------------------------------------------------------------------------Shop----------------------------------------------------------
 @app.post("/newitem", response_model=Item)
 def add_item(item: Item) -> Item:
@@ -104,17 +109,25 @@ def add_item(item: Item) -> Item:
         item.dict()
     ).inserted_id
     
-    # mongodb_client.inventory.shop.update_one(
-    #         {'_id': inserted_id}, {'$set':{'id':inserted_id}})
-    
     new_item = Item(
         **mongodb_client.inventory.shop.find_one(
             {"_id": ObjectId(inserted_id)}
         )
     )
     #logging.info(f"✨ New team created: {new_team}")
-    #emit_events.send(inserted_id, "create", new_team.dict())
+    emit_events.send(inserted_id, "create", new_item.dict())
     return new_item
+
+@app.get("/market", response_model=list[Item])
+def get_all_items(id: list[int] = Query(None))-> list[Item]:
+    filters = dict()
+    if id:
+        filters['_id'] = {"$in": [ObjectId(_id) for _id in id]}
+
+    shop_items = [Item(**item).dict() for item in mongodb_client.inventory.shop.find(filters)]
+
+    return shop_items
+    
 
 @app.get("/getitem/{item_id}", response_model=Item)
 def get_item(item_id: str)-> Item:
@@ -130,10 +143,10 @@ def get_item(item_id: str)-> Item:
 def update_item(item_id: str, item: dict)-> Item:
     try:
         item_id = ObjectId(item_id)
-        mongodb_client.inventory.shop.update_one(
+        item = mongodb_client.inventory.shop.update_one(
             {'_id': item_id}, {"$set": item})
 
-        #emit_events.send(player_id, "update", player)
+        emit_events.send(item_id, "update", item)
 
         return Item(
             **mongodb_client.inventory.shop.find_one({"_id": item_id})
@@ -156,7 +169,7 @@ def delete_item(item_id: str) -> str:
         {"_id": ObjectId(item_id)}
     )
 
-    #emit_events.send(player_id, "delete", player.dict())
+    emit_events.send(item_id, "delete", item.dict())
     return 'ok'
 #--------------------------------------------------------------------------Functionalities----------------------------------------------------------
 #updates the items of a user, new_items is a list of Item
@@ -167,12 +180,12 @@ def update_user_items(user_id: str, new_items: list[dict]) -> User:
         user_id = ObjectId(user_id)
         mongodb_client.inventory.users.update_one(
             {'_id': user_id}, {'$set':{'items':new_items}})
-
-        #emit_events.send(player_id, "update", player)
-
-        return User(
+        user = User(
             **mongodb_client.inventory.users.find_one({"_id": user_id})
         )
+        emit_events.send(user_id, "update", user.dict())
+
+        return user
 
     except (InvalidId, TypeError):
         raise HTTPException(status_code=404, detail="User not found")
@@ -180,72 +193,74 @@ def update_user_items(user_id: str, new_items: list[dict]) -> User:
 #adds a specific quantity of one item to one user
 @app.post("/{user_id}/{item_id}/{quantity}", response_model=User)
 def add_item_to_user(user_id: str, item_id: str, quantity:int, action:int) -> User:
-    user = get_user(user_id)
-    old_items = user.items
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-    print(old_items)
-    found = False
-    for indice in range(len(old_items)):
-        if old_items[indice].id == item_id: #si ya lo tiene, suma la cantidad
-            found=True
-            break
-    if found:
-        old_items[indice].quantity = old_items[indice].quantity + quantity*action
-            #old_items[indice]=old_items[indice].__dict__
-        if old_items[indice].quantity == 0:
-            old_items.pop(indice)
-        else:
-            old_items[indice]=old_items[indice].__dict__
-        
-    else: #si no lo tiene, lo agrega al final de la lista de items
-        new_item = get_item(item_id)
-        new_item.quantity=quantity
-        new_item = new_item.__dict__
-        old_items.append(new_item)
-    new=[]
-    for x in old_items:
-        x=x.__dict__
-        new.append(x)
-    user2 = update_user_items(user_id, new)
-    return user2
+    if quantity<=0:
+        raise HTTPException(status_code=400, detail="Invalid quantity")
+    else:
+        user = get_user(user_id)
+        old_items = user.items
+        found = False
+        for indice in range(len(old_items)):
+            if old_items[indice].id == item_id: #si ya lo tiene, suma la cantidad
+                found=True
+                break
+        if found:
+            old_items[indice].quantity = old_items[indice].quantity + quantity*action
+                #old_items[indice]=old_items[indice].__dict__
+            if old_items[indice].quantity == 0:
+                old_items.pop(indice)
+            
+        else: #si no lo tiene, lo agrega al final de la lista de items
+            new_item = get_item(item_id)
+            new_item.quantity=quantity
+            old_items.append(new_item)
 
+        new=[] #to transform into dictionary
+        for x in old_items:
+            x=x.dict()
+            new.append(x)
+        user2 = update_user_items(user_id, new)
 
+        emit_events.send(user_id, "update", user2.dict())
+        return user2
 
 @app.post("/buy/{user_id}/{item_id}/{quantity}", response_model=int)
 async def buy(user_id: str, item_id: str, quantity: int) -> int:
-    #sacar el costo de 1 item, calcular costo total compra
-    total_cost = quantity*get_item(item_id).buy_price
-    #revisar si el usuario tiene disponible ese dinero
-    if get_user(user_id).gold < total_cost :
-        #error no hay plata
-        return 0
+    if quantity<=0:
+        raise HTTPException(status_code=400, detail="Invalid quantity")
     else:
-        #agregar item
-        user = add_item_to_user(user_id, item_id, quantity,1)
-        #quitar dinero
-        new_gold = user.gold - total_cost
-        user_id = ObjectId(user_id)
-        mongodb_client.inventory.users.update_one(
-            {'_id': user_id}, {"$set": {'gold':new_gold}})
-        return 1
+        total_cost = quantity*get_item(item_id).buy_price
+        if get_user(user_id).gold < total_cost :
+            #error no hay plata
+            return 0
+        else:
+            payload = {'user_id':user_id, 'item_id': item_id, 'quantity': quantity, 'gold_spent': total_cost}
+            user = add_item_to_user(user_id, item_id, quantity,1)
+            new_gold = user.gold - total_cost
+            user_id = ObjectId(user_id)
+            mongodb_client.inventory.users.update_one(
+                {'_id': user_id}, {"$set": {'gold':new_gold}})
+            
+            emit_events.send(user_id, "purchase", payload)
+            return 1
 
 @app.post("/sell/{user_id}/{item_id}/{quantity}", response_model=int)
 async def sell(user_id: str, item_id: str, quantity: int) -> int:
-    #revisar que tenga al menos quantity del item que quiere vender
-    user = get_user(user_id)
-    for item in user.items:
-        if item.id == item_id:
-            if quantity > item.quantity: #no hay suficiente cantidad
-                print('holi')
-                return 0
-            else:
-                #delete items from user's inventory
-                user = add_item_to_user(user_id, item_id, quantity,-1)
-                #add gold
-                price = quantity*get_item(item_id).sell_price
-                new_gold = user.gold + price
-                user_id = ObjectId(user_id)
-                mongodb_client.inventory.users.update_one(
-                    {'_id': user_id}, {"$set": {'gold':new_gold}})
-                return 1
-    return 0
+    if quantity<=0:
+        raise HTTPException(status_code=400, detail="Invalid quantity")
+    else:
+        user = get_user(user_id)
+        for item in user.items:
+            if item.id == item_id:
+                if quantity > item.quantity: 
+                    return 0
+                else:
+                    user = add_item_to_user(user_id, item_id, quantity,-1)
+                    price = quantity*get_item(item_id).sell_price
+                    payload = {'user_id':user_id, 'item_id': item_id, 'quantity': quantity, 'gold_earned': price}
+                    new_gold = user.gold + price
+                    user_id = ObjectId(user_id)
+                    mongodb_client.inventory.users.update_one(
+                        {'_id': user_id}, {"$set": {'gold':new_gold}})
+                    emit_events.send(user_id, "purchase", payload)
+                    return 1
+        return 0
