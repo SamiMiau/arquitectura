2e2b4c51f63b4e1d86755d8f6eec2668659452f9
unknown <natalia.sanchez@sansano.usm.cl>
2023-11-12 19:28:17 -0300

    omg atlas worked

--- app/events.py
+++ app/events.py
@@ -11,7 +11,7 @@ class Emit:
 
     def connect(self):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='inventory_message_broker')
+            pika.ConnectionParameters(host='rabbitmq')
         )
 
         self.channel = self.connection.channel()
@@ -29,35 +29,3 @@ class Emit:
     def close(self):
         self.connection.close()
 
-
-# class Receive:
-#     def __init__(self):
-#         self.connection = pika.BlockingConnection(
-#             pika.ConnectionParameters(host='demo_04_message_broker')
-#         )
-
-#         self.channel = self.connection.channel()
-#         self.channel.exchange_declare(exchange='players',
-#                                       exchange_type='topic')
-
-#         self.channel.queue_declare('team_for_player_queue', exclusive=True)
-#         self.channel.queue_bind(exchange='players',
-#                                 queue="team_for_player_queue",
-#                                 routing_key="player.delete.*")
-
-#         self.channel.basic_consume(queue='team_for_player_queue',
-#                                    on_message_callback=self.callback)
-
-#         self.channel.start_consuming()
-
-#     def callback(self, ch, method, properties, body):
-#         body = json.loads(body)
-#         logging.info(f"Good by {body['name']} 👋")
-#         ch.basic_ack(delivery_tag=method.delivery_tag)
-
-#     def close(self):
-#         self.connection.close()
-
-
-# if __name__ == '__main__':
-#     Receive()
