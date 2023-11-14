daec68773585ef64ece320809ce738f11c794557
unknown <natalia.sanchez@sansano.usm.cl>
2023-11-13 22:46:33 -0300

    final commit hopefully

--- app/test_main.py
+++ app/test_main.py
@@ -16,7 +16,7 @@ def test_read_main():
 #     response = client.get("/newuser/{user_name}")
 #     assert response.status_code == 200
 #     assert response.json() == {"msg": "Hello World"}
-    
+
 # def test_get_user():
 #     response = client.get("/getuser/{user_id}")
 #     assert response.status_code == 200
@@ -37,13 +37,13 @@ def test_get_all_inventory_successful():
     "name": "Strawberry",
     "buy_price": 50,
     "sell_price": 10,
-    "quantity": 5,
+    "quantity": 4,
     "description": "Nice and sweet!!"
   },
   {
     "id": "65516da0d1694e37ed09683f",
     "name": "Pear",
-    "buy_price": 100,
+    "buy_price": 999,
     "sell_price": 50,
     "quantity": 2,
     "description": "Perame tantito"
@@ -59,20 +59,34 @@ def test_get_all_inventory_unsuccessful_404():
 
 #  ------------------------------------------------------------------ Shop tests
 
-def test_add_item():
+def test_add_item_allparameters():
     response = client.post("/newitem", json={
-        "name": "TestItem",
+        "name": "TestItem1",
         "buy_price": 10000,
         "sell_price": 500,
         "description": "Test Description"
     })
     assert response.status_code == 200
     data = response.json()
-    assert data["name"] == "TestItem"
+    assert data["name"] == "TestItem1"
     assert data["buy_price"] == 10000
     assert data["sell_price"] == 500
     assert data["quantity"] == 1
     assert data["description"] == "Test Description"
+def test_add_item_someparameters():
+    response = client.post("/newitem", json={
+        "name": "TestItem2",
+        "buy_price": 10000,
+        "sell_price": 500,
+        "quantity": 1,
+    })
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "TestItem2"
+    assert data["buy_price"] == 10000
+    assert data["sell_price"] == 500
+    assert data["quantity"] == 1
+    assert data["description"] == "stuff!!"
 
 def test_get_all_items_successful_all():
     response = client.get("/market")
@@ -93,7 +107,7 @@ def test_get_all_items_successful_list():
   {
     "id": "65516da0d1694e37ed09683f",
     "name": "Pear",
-    "buy_price": 100,
+    "buy_price": 999,
     "sell_price": 50,
     "quantity": 1,
     "description": "Perame tantito"
@@ -106,49 +120,189 @@ def test_get_all_items_unsuccessful_list():
         "detail": "Items not found"
     }
 
-# def test_get_item():
-#     response = client.get("/getitem/{item_name}")
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "Hello World"}
+def test_get_item_successful():
+    response = client.get("/getitem/Strawberry")
+    assert response.status_code == 200
+    assert response.json() == {
+      "id": "65516ccbd1694e37ed09683e",
+      "name": "Strawberry",
+      "buy_price": 50,
+      "sell_price": 10,
+      "quantity": 1,
+      "description": "Nice and sweet!!"
+    }
+def test_get_item_unsuccessful_404():
+    response = client.get("/getitem/Frutilla")
+    assert response.status_code == 404
+    assert response.json() == {
+      "detail": "Item not found"
+    }
 
-# def test_update_item():
-#     response = client.get("/updateitem/{item_name}")
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "Hello World"}
+def test_update_item():
+    response = client.put("/updateitem/Pear", json = {
+    "name": "Pear",
+    "buy_price": 1000,
+    "sell_price": 50,
+    "quantity": 1,
+    "description": "Perame tantito"
+    })
+    assert response.status_code == 200
+    assert response.json() == {
+    "id": "65516da0d1694e37ed09683f",
+    "name": "Pear",
+    "buy_price": 1000,
+    "sell_price": 50,
+    "quantity": 1,
+    "description": "Perame tantito"
+    }
+    response2 = client.put("/updateitem/Pear", json = {
+    "name": "Pear",
+    "buy_price": 999,
+    "sell_price": 50,
+    "quantity": 1,
+    "description": "Perame tantito"
+    })
+    assert response2.status_code == 200
+    assert response2.json() == {
+      "id": "65516da0d1694e37ed09683f",
+      "name": "Pear",
+      "buy_price": 999,
+      "sell_price": 50,
+      "quantity": 1,
+      "description": "Perame tantito"
+    }
+def test_update_item_unsuccessful_404():
+    response = client.put("/updateitem/Perita", json = {
+    "name": "Perita",
+    "buy_price": 1000,
+    "sell_price": 50,
+    "quantity": 1,
+    "description": "Perame tantito plss"
+    })
+    assert response.status_code == 404
+    assert response.json() == {
+    "detail": "Item not found"
+    }
 
-# def test_delete_item():
-#     response = client.get("/deleteitem/{item_name}")
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "Hello World"}
+def test_delete_item():
+    response = client.post("/newitem", json={
+        "name": "DeleteTestItem",
+        "buy_price": 10000,
+        "sell_price": 500,
+        "description": "Test Description"
+    })
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "DeleteTestItem"
+    assert data["buy_price"] == 10000
+    assert data["sell_price"] == 500
+    assert data["quantity"] == 1
+    assert data["description"] == "Test Description"
+
+    response2 = client.delete("/deleteitem/DeleteTestItem")
+    assert response2.status_code == 200
+    assert response2.json() == "ok"
+def test_delete_item_unsuccessful_404():
+    response2 = client.delete("/deleteitem/DeleteTestItem2")
+    assert response2.status_code == 404
+    assert response2.json() == {
+    "detail": "Item not found"
+    }
 
 # #  ------------------------------------------------------------------ Items tests
 
-# def test_update_user_items():
-#     response = client.get("/updateuseritems/{user_id}")
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "Hello World"}
+def test_update_user_items():
+    response = client.put("/updateuseritems/65516c90d1694e37ed09683d", json=[{
+        "id": "65516ccbd1694e37ed09683e",
+        "name": "Strawberry",
+        "buy_price": 50,
+        "sell_price": 10,
+        "quantity": 100,
+        "description": "Nice and sweet!!"
+        }])
+    assert response.status_code == 200
+    assert response.json() == {
+    "id": "65516c90d1694e37ed09683d",
+    "name": "Jacinta",
+    "gold": 1000,
+    "items": [
+      {
+      "id": "65516ccbd1694e37ed09683e",
+      "name": "Strawberry",
+      "buy_price": 50,
+      "sell_price": 10,
+      "quantity": 100,
+      "description": "Nice and sweet!!"
+    }
+    ]
+    }
+def test_update_user_items_unsuccessful_404():
+    response = client.put("/updateuseritems/76516c90d1694e37ed09683d", json=[{
+        "id": "65516ccbd1694e37ed09683e",
+        "name": "Strawberry",
+        "buy_price": 50,
+        "sell_price": 10,
+        "quantity": 100,
+        "description": "Nice and sweet!!"
+        }])
+    assert response.status_code == 404
+    assert response.json() == {
+    "detail": "User not found"
+    }
 
-# def test_add_item_to_user():
-#     response = client.get("/{user_id}/{item_name}/{quantity}")
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "Hello World"}
+def test_add_item_to_user():
+    response1 = client.put("/updateuseritems/65522021e80ede849e09d703", json=[])
+    response = client.post("/65522021e80ede849e09d703/Strawberry/1?action=1")
+    assert response.status_code == 200
+    assert response.json() == {
+    "id": "65522021e80ede849e09d703",
+    "name": "Facundo",
+    "gold": 1000,
+    "items": [
+      {
+      "id": "65516ccbd1694e37ed09683e",
+      "name": "Strawberry",
+      "buy_price": 50,
+      "sell_price": 10,
+      "quantity": 1,
+      "description": "Nice and sweet!!"
+      }
+    ]
+  }
+def test_add_item_to_user_unsuccessful_404():
+    response = client.post("/76522021e80ede849e09d703/Strawberry/1?action=1")
+    assert response.status_code == 404
+    assert response.json() == {
+    "detail": "User not found"
+    }
 
 # #  ------------------------------------------------------------------ User actions tests
 
-# def test_buy_successful():
-#     response = client.get("/buy/{user_id}/{item_name}/{quantity}")
-#     assert response.status_code == 200
-#     assert response.json() == 1
-# def test_buy_unsuccessful():
-#     response = client.get("/buy/{user_id}/{item_name}/{quantity}")
-#     assert response.status_code == 200
-#     assert response.json() == 0
+def test_buy_successful():
+    response = client.post("/buy/655150ba2232f506445756a4/Strawberry/1")
+    assert response.status_code == 200
+    assert response.json() == 1
+def test_buy_unsuccessful_notenoughgold():
+    response = client.post("/buy/655150ba2232f506445756a4/Strawberry/1000")
+    assert response.status_code == 200
+    assert response.json() == 0
 
-# def test_sell_successful():
-#     response = client.get("/sell/{user_id}/{item_name}/{quantity}")
-#     assert response.status_code == 200
-#     assert response.json() == 1
-# def test_sell_unsuccessful():
-#     response = client.get("/sell/{user_id}/{item_name}/{quantity}")
-#     assert response.status_code == 200
-#     assert response.json() == 0
+def test_sell_successful():
+    response = client.post("/sell/655150ba2232f506445756a4/Strawberry/1")
+    assert response.status_code == 200
+    assert response.json() == 1
+def test_sell_unsuccessful():
+    response = client.post("/sell/655150ba2232f506445756a4/Strawberry/1000")
+    assert response.status_code == 200
+    assert response.json() == 0
+
+
+# #  ------------------------------------------------------------------ Delete testing item remains
+def test_delete_item():
+    response1 = client.delete("/deleteitem/DeleteTestItem")
+    response2 = client.delete("/deleteitem/TestItem")
+    response3 = client.delete("/deleteitem/TestItem1")
+    response4 = client.delete("/deleteitem/TestItem2")
+    response5 = client.delete("/deleteitem/TestItem1")
+    response6 = client.delete("/deleteitem/TestItem2")
+    assert response3.status_code == 200
